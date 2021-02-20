2014cf95b81e2831fdc82fd59069ea78bdbfd158
hanafa01 <hana_fakhouri@hotmail.com>
2021-02-20 22:56:07 +0300

    first method to connect to db

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -3,7 +3,13 @@
 class User_model extends CI_Model{
 
     public function get_users(){
-        
+        $config['hostname'] = 'localhost';
+        $config['username'] = 'root';
+        $config['password'] = '';
+        $config['database'] = 'codeigniter_errand_db';
+
+        $connection = $this->load->database($config);
+
     }
 
 }
