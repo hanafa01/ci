686dd876ce7f54009da89e38640e833fd679f487
hanafa01 <hana_fakhouri@hotmail.com>
2021-02-20 22:59:46 +0300

    second  method to connect to db and get the users

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -3,12 +3,15 @@
 class User_model extends CI_Model{
 
     public function get_users(){
-        $config['hostname'] = 'localhost';
-        $config['username'] = 'root';
-        $config['password'] = '';
-        $config['database'] = 'codeigniter_errand_db';
+        // $config['hostname'] = 'localhost';
+        // $config['username'] = 'root';
+        // $config['password'] = '';
+        // $config['database'] = 'codeigniter_errand_db';
 
-        $connection = $this->load->database($config);
+        // $connection = $this->load->database($config);
+
+        $query = $this->db->get('users');
+        return $query->result();
 
     }
 
