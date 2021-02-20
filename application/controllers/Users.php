e738dbd99242e5841a121cef68b61f6046954f58
hanafa01 <hana_fakhouri@hotmail.com>
2021-02-20 23:06:52 +0300

    processing a result from a model in a controller

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -5,6 +5,10 @@ class Users extends CI_Controller{
     public function show(){
         //$this->load->model('User_model');
         $result = $this->User_model->get_users();
+
+        foreach($result as $object){
+            echo $object->username."<br/>";
+        }
     }
 
 }
