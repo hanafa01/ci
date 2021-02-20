7ada65097fbf34e29d58ed70c9ac80ea0a7903e9
hanafa01 <hana_fakhouri@hotmail.com>
2021-02-20 23:12:21 +0300

    pass static data to a view

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -6,9 +6,13 @@ class Users extends CI_Controller{
         //$this->load->model('User_model');
         $result = $this->User_model->get_users();
 
-        foreach($result as $object){
-            echo $object->username."<br/>";
-        }
+        // foreach($result as $object){
+        //     echo $object->username."<br/>";
+        // }
+
+        $data['welcome'] = "Welcome to my page";
+
+        $this->load->view('user_view', $data);
     }
 
 }
