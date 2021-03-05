2a6c78d1b12aa9d404e1c9477da69a744a6d5169
hanafa01 <hana_fakhouri@hotmail.com>
2021-03-05 15:14:14 +0300

    access to projects page

--- application/controllers/Projects.php
+++ application/controllers/Projects.php
@@ -2,6 +2,14 @@
 
 class Projects extends CI_Controller{
 
+    public function  __construct(){
+        parent::__construct();
+        if(!$this->session->userdata('logged_in')){
+            $this->session->set_flashdata('error_message', 'Sorry You have to login in order to see the projects page.');
+            redirect('/');
+        }
+    }
+
     public function index(){
         $data['main_view'] = "projects/index";
         $this->load->view('layouts/main', $data);
