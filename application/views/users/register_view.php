1e036e7f197400f37ab220c22cb65e9b9e8bd922
hanafa01 <hana_fakhouri@hotmail.com>
2021-04-11 12:44:29 +0300

    set value in form to display input field again if error instead of rewriting it

--- application/views/users/register_view.php
+++ application/views/users/register_view.php
@@ -7,22 +7,22 @@
 <?php echo form_open('users/register', $attributes);?>
     <div class="form-group">
         <?php echo form_label('First Name'); ?>
-        <?php $data = array('class' => 'form-control', 'name' => 'first_name', 'placeholder' => 'Enter First Name'); ?>
+        <?php $data = array('class' => 'form-control', 'name' => 'first_name', 'placeholder' => 'Enter First Name', 'value' => set_value('first_name')); ?>
         <?php echo form_input($data); ?>
     </div>
     <div class="form-group">
         <?php echo form_label('Last Name'); ?>
-        <?php $data = array('class' => 'form-control', 'name' => 'last_name', 'placeholder' => 'Enter Last Name'); ?>
+        <?php $data = array('class' => 'form-control', 'name' => 'last_name', 'placeholder' => 'Enter Last Name', 'value' => set_value('last_name')); ?>
         <?php echo form_input($data); ?>
     </div>
     <div class="form-group">
         <?php echo form_label('Email'); ?>
-        <?php $data = array('class' => 'form-control', 'name' => 'email', 'placeholder' => 'Enter Email'); ?>
+        <?php $data = array('class' => 'form-control', 'name' => 'email', 'placeholder' => 'Enter Email', 'value' => set_value('email')); ?>
         <?php echo form_input($data); ?>
     </div>
     <div class="form-group">
         <?php echo form_label('Username'); ?>
-        <?php $data = array('class' => 'form-control', 'name' => 'username', 'placeholder' => 'Enter Username'); ?>
+        <?php $data = array('class' => 'form-control', 'name' => 'username', 'placeholder' => 'Enter Username', 'value' => set_value('username')); ?>
         <?php echo form_input($data); ?>
     </div>
     <div class="form-group">
